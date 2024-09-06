0772f96c867e367d957b9f1796597df3ab5e23f8
Hithesh <hithesh@Hitheshs-MacBook-Air.local>
2024-09-07 02:30:03 +0530

    starter protfolio

---
+++ src/data/resume.tsx
@@ -0,0 +1,119 @@
+import { Icons } from "@/components/icons";
+import { HomeIcon, NotebookIcon } from "lucide-react";
+
+export const DATA = {
+  name: "Hithesh R",
+  initials: "DV",
+  url: "https://hithesh.live",
+  location: "Bengaluru India",
+  locationLink: "https://www.google.com/maps/place/sanfrancisco",
+  description:
+    "Software Engineer . I love building things and helping people. Very active on Twitter.",
+  summary:
+    "At the end of 2022, I quit my job as a software engineer to go fulltime into building and scaling my own SaaS businesses. In the past, [I pursued a double degree in computer science and business](/#education), [interned at big tech companies in Silicon Valley](https://www.youtube.com/watch?v=d-LJ2e5qKdE), and [competed in over 21 hackathons for fun](/#hackathons). I also had the pleasure of being a part of the first ever in-person cohort of buildspace called [buildspace sf1](https://buildspace.so/sf1).",
+  avatarUrl: "/me.png",
+  skills: [
+    "React",
+    "Next.js",
+    "Typescript",
+    "Node.js",
+    "Python",
+    "Postgres",
+    "Docker",
+  ],
+  navbar: [
+    { href: "/", icon: HomeIcon, label: "Home" },
+    { href: "/blog", icon: NotebookIcon, label: "Blog" },
+  ],
+  contact: {
+    email: "hello@example.com",
+    tel: "+123456789",
+    social: {
+      GitHub: {
+        name: "GitHub",
+        url: "https://dub.sh/dillion-github",
+        icon: Icons.github,
+
+        navbar: true,
+      },
+      LinkedIn: {
+        name: "LinkedIn",
+        url: "https://dub.sh/dillion-linkedin",
+        icon: Icons.linkedin,
+
+        navbar: true,
+      },
+      X: {
+        name: "X",
+        url: "https://dub.sh/dillion-twitter",
+        icon: Icons.x,
+
+        navbar: true,
+      },
+      Youtube: {
+        name: "Youtube",
+        url: "https://dub.sh/dillion-youtube",
+        icon: Icons.youtube,
+        navbar: true,
+      },
+      email: {
+        name: "Send Email",
+        url: "#",
+        icon: Icons.email,
+
+        navbar: false,
+      },
+    },
+  },
+
+  work: [
+    {
+      company: "Frelancer",
+      href: "https://atomic.finance",
+      badges: [],
+      location: "Remote",
+      title: "Bitcoin Protocol Engineer",
+      logoUrl: "/atomic.png",
+      start: "May 2021",
+      end: "Oct 2022",
+      description:
+        "Implemented the Bitcoin discreet log contract (DLC) protocol specifications as an open source Typescript SDK. Dockerized all microservices and setup production kubernetes cluster. Architected a data lake using AWS S3 and Athena for historical backtesting of bitcoin trading strategies. Built a mobile app using react native and typescript.",
+    },
+  ],
+  education: [
+    {
+      school: "BMSCE",
+      href: "https://www.bmsce.ac.in/",
+      degree: "Bachelors of Engineeering",
+      logoUrl: "/",
+      start: "2022",
+      end: "2026",
+    },
+  ],
+  projects: [
+    {
+      title: "No Sign",
+      href: "https://nosign.vercel.app",
+      dates: "Jan 2024 - Feb 2024",
+      active: true,
+      description: "With the release of the .",
+      technologies: [
+        "Next.js",
+        "Typescript",
+        "Redis",
+        "Prisma",
+        "TailwindCSS",
+        "Shadcn UI",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://nosign.vercel.com",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/s.png",
+    },
+  ],
+  hackathons: [],
+} as const;
